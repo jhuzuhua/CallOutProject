2618f44c3f983372b93b906a81578098a80ae57d
Joshua N. Paldo <joshuapaldo@yahoo.com>
2015-06-28 14:26:59 +0800

    First commit.

---
+++ fco/FCOAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fco
+//
+//  Created by Kryptonite on 4/7/15.
+//  Copyright (c) 2015 Kryptonite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fco/FCOLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  FCOLoginViewController.h
+//  fco
+//
+//  Created by kryptonite on 6/1/15.
+//  Copyright (c) 2015 Kryptonite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCOLoginViewController : UIViewController
+
+@end
